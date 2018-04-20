cbc3f53e72088db162166743e6a763b3a236af6b
ark9719 <ark9719@rit.edu>
2018-04-20 00:15:12 -0400

    First pass at a model. I know this has more than two classes but I want to see if it runs

---
+++ train.py
@@ -0,0 +1,83 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+
+
+#Data params
+img_width, img_height = 320, 240
+train_data_dir = 'data/TRAIN'
+validation_data_dir = 'data/TEST'
+classes = ['EOSINOPHIL', 'LYMPHOCYTE', 'MONOCYTE', 'NEUTROPHIL']
+
+
+#Training params
+epochs = 50
+batch_size = 16
+
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height)
+
+#Model
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentrpy',
+            optimizer='rmsprop',
+            metrics=['accuracy'])
+
+
+#Train and test data augmentors
+train_datagen = ImageDataGenerator (
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+
+#Generators from TEST and TRAIN
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+test_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch= 2000 // batch_size,
+    epochs=epochs,
+    validation_data=test_generator,
+    validation_steps= 800 // batch_size
+)
+
+model.save_weights('first_try.h5')
